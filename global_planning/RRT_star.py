d7d8d177d7cb614e37f61d4ec380391397b3908f
Willem Momma <willemmomma@gmail.com>
2022-12-12 11:45:13 +0100

    added sandbox.py for debugging purposes and created first working version of the tree builder in the RRT_star class

--- global_planning/RRT_star.py
+++ global_planning/RRT_star.py
@@ -2,14 +2,16 @@
 import random 
 import math
 import matplotlib.pyplot as plt
-class Node:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.path_x = []
-        self.path_y = []
-        self.parent = None
+from shapely.geometry import Polygon, Point, LineString
+
+
+
 class obstacleSquare:
+    '''
+    Class for squared obstacles
+    Input:  x, y: center of obstacle
+            size: length of side of obstacle
+    '''
     def __init__(self, x, y, size):
         self.x = x
         self.y = y
@@ -18,23 +20,30 @@ class obstacleSquare:
         self.x2 = x + size/2
         self.y1 = y - size/2
         self.y2 = y + size/2
-class obstacleCircle:
-    def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
-
 class RRT_star:
     ''' 
     Class for RRT star planning 
-    Goal: find a path from start to goal avoiding obstacles
+    Goal: find optimal path from start to goal avoiding obstacles
     Returns: path as a list of nodes [[x1, y1], [x2, y2], ...]
     Also plots the growing tree, final path and obstacles
     '''
-    def __init__(self, start, goal, obstacleList, randArea, maxIter=500, neighborRadius=50):
+    class Node:
+        '''
+        Class to define the Nodes (i.e. vertices)
+        Also specifies parent Node and path from parent to child 
+        Input:  x, y: node coordinates
+        '''
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+            self.path_x = []
+            self.path_y = []
+            self.parent = None
+
+    def __init__(self, start, goal, obstacleList, randArea):
         '''
         Initialize the class variables
-        Input: start: start node
+        Input:  start: start node
                 goal: goal node
                 obstacleList: list of obstacles as polygons [obstacle1, obstacle2, ...]
                 randArea: random sampling area equal to map size
@@ -42,15 +51,13 @@ class RRT_star:
         Output: None
         '''
 
-        self.start = Node(start[0], start[1])
-        self.end = Node(goal[0], goal[1])
+        self.start = self.Node(start[0], start[1])
+        self.end = self.Node(goal[0], goal[1])
         self.minRand = randArea[0]
         self.maxRand = randArea[1]
-        self.maxIter = maxIter
+        self.maxIter = 100
         self.obstacleList = obstacleList
-        self.neighborRadius = neighborRadius
-        self.expandDistance = 30.0 #distance by which nodes are expanded towards random points
-
+        
     def planning(self):
         '''
         Path planning using RRT star algorithm
@@ -61,28 +68,32 @@ class RRT_star:
         '''
         self.nodeList = [self.start] # initialize node list with start node
         for i in range(self.maxIter):
-            qRand = self.getRandomPoint() #sample random point
-            nearestNode = self.getNearestNode(self.nodeList, qRand) #find nearest node from random point to existing nodes
-            newNode = self.steeringFunction(nearestNode, qRand) #create new node by steering towards random point
-            if self.checkCollision(newNode, self.obstacleList): #returns True if no collision
-                neighbors = self.findNeighbors(newNode) #find neighbors within a radius, neighbor_radius
-                nodeUpdatedParent = self.chooseParent(newNode, neighbors) #returns None if no parent is found, if parent is found, returns the node with updated parent
-                if nodeUpdatedParent: #returns True if parent is found
-                    self.rewire(nodeUpdatedParent, neighbors) #rewire the tree
-                    self.nodeList.append(nodeUpdatedParent) #add the node to the node list
-        path = self.generateFinalPath(self.nodeList, self.end) #generate final path
+            qRand = self.getRandomPoint()
+            if qRand == None:
+                continue
+            else:
+                nearestNode = self.getNearestNode(self.nodeList, qRand) #find nearest node from random point to existing nodes            
+                newNode = self.steeringFunction(nearestNode, qRand) #create new node by steering towards random point
+                if self.lineCollisionCheck(newNode, self.obstacleList):
+                    self.nodeList.append(newNode)
+                    self.plotGraph(self.nodeList)
+        plt.show()
+        path = self.nodeList
         return path
 
     def getRandomPoint(self):
         '''
-        Samples random node within the random sampling area
+        Samples random node from the collision free configuration space 
         Input: None
         Output: q_rand: random node
         '''
         randomx = random.uniform(self.minRand, self.maxRand)
         randomy = random.uniform(self.minRand, self.maxRand)
-        q_rand = self.Node(randomx, randomy)
-        return q_rand
+        for obs in self.obstacleList:
+            if randomx >= obs.x1 and randomx <= obs.x2 and randomy >= obs.y1 and randomy <= obs.y2:
+                return None
+        qRand = self.Node(randomx, randomy)
+        return qRand
     
     def euclideanDistance(self, node1, node2):
         '''
@@ -111,89 +122,44 @@ class RRT_star:
                 toNode: node towards which steering is done
         Output: newNode: node after steering, includes path from fromNode to newNode
         '''
-        newNode = self.Node(fromNode.x, fromNode.y)
-        newNode.path_x = [newNode.x]
-        newNode.path_y = [newNode.y]
-        theta = math.atan2(toNode.y - fromNode.y, toNode.x - fromNode.x) #angle between fromNode and toNode
-        newNode.x += self.expandDistance * math.cos(theta) #
-        newNode.y += self.expandDistance * math.sin(theta)
-        newNode.path_x.append(newNode.x)
-        newNode.path_y.append(newNode.y)
+        newNode = self.Node(toNode.x, toNode.y)
+        newNode.path_x = [fromNode.x, toNode.x]
+        newNode.path_y = [fromNode.y, toNode.y]
         newNode.parent = fromNode
-        return newNode
+        
 
-    def checkCollision(node, obstacleList):
+        return newNode
+    
+    def lineCollisionCheck(self, newNode, obstacleList):
         '''
-        Check if node collides with any obstacle
-        Input:  node: node to be checked for collision
+        Check if line between two nodes collides with any obstacle
+        Input:  node1: start node
+                node2: end node
                 obstacleList: list of obstacles as polygons [obstacle1, obstacle2, ...]
         Output: True if no collision, False if collision
         '''
-        for (obstacle) in obstacleList:
-            if isinstance(obstacle, obstacleSquare):
-                if node.x >= obstacle.x1 and node.x <= obstacle.x2 and node.y >= obstacle.y1 and node.y <= obstacle.y2:
-                    return False
-            elif isinstance(obstacle, obstacleCircle):
-                if math.sqrt((node.x - obstacle.x)**2 + (node.y - obstacle.y)**2) <= obstacle.radius:
-                    return False
+        #generate line object 
+        line = LineString([(newNode.path_x[0], newNode.path_y[0]), (newNode.path_x[1], newNode.path_y[1])])
+        for obs in obstacleList:
+            polygon = Polygon([(obs.x1, obs.y1), (obs.x2, obs.y1), (obs.x2, obs.y2), (obs.x1, obs.y2)])
+            if polygon.intersects(line):
+                return False
         return True
 
     def findNeighbors(self, newNode):
-        '''
-        Find all nodes within a radius, neighbor_radius
-        Input:  node: node for which neighbors are to be found
-                nodeList: list of all nodes
-        Output: neighbors: list of all nodes within a radius, neighbor_radius
-        '''
-        neighbors = []
-        for node in self.nodeList:
-            if self.euclideanDistance(node, newNode) <= self.neighborRadius:
-                neighbors.append(node)
-        return neighbors
+        pass
     
     def chooseParent(self, node, neighbors):
-        '''
-        Choose parent for a node, best parent is the one with minimum cost from start to node
-        Input:  node: node
-                neighbors: list of all nodes within a radius, neighbor_radius
-        Output: node: node with updated parent
-        '''
-        if neighbors:
-            costs = [self.euclideanDistance(node, neighbor) + neighbor.cost for neighbor in neighbors]
-            minCostIndex = costs.index(min(costs))
-            node.parent = neighbors[minCostIndex]
-            node.cost = costs[minCostIndex]
-            return node
-        return None
+        pass
 
     def rewire(self, node, neighbors):
-        '''
-        Rewire the tree, if a node has a better parent, change the parent
-        Input:  node: node
-                neighbors: list of all nodes within a radius, neighbor_radius
-        Output: None
-        '''
-        for neighbor in neighbors:
-            if self.euclideanDistance(node, neighbor) + node.cost < neighbor.cost:
-                neighbor.parent = node
-                neighbor.cost = self.euclideanDistance(node, neighbor) + node.cost
+        pass
 
     def generateFinalPath(self, nodeList, goal):
-        '''
-        Generate final path from start to goal
-        Input:  nodeList: list of all nodes
-                goal: goal node
-        Output: path: list of nodes from start to goal
-        '''
-        path = []
-        node = goal
-        while node.parent is not None:
-            path.append(node)
-            node = node.parent
-        path.append(node)
-        return path
+        pass
+
         
-    def plotGraph(self, nodeList, path):
+    def plotGraph(self, nodeList):
         '''
         Plot the graph
         Input:  nodeList: list of all nodes
@@ -201,30 +167,36 @@ class RRT_star:
         Output: None
         '''
         plt.clf()
+        plt.plot([self.start.x], self.start.y, 'ro')
+        plt.plot([self.end.x],self.end.y, 'go')
+
         for node in nodeList:
+            plt.plot(node.x,node.y, 'yo')
             if node.parent:
-                plt.plot(node.path_x, node.path_y, '-g')
-        for node in path:
-            plt.plot(node.path_x, node.path_y, '-r')
-        plt.plot(self.start.x, self.start.y, "xr")
-        plt.plot(self.end.x, self.end.y, "xr")
-        plt.axis([-2, 15, -2, 15])
+                plt.plot(node.path_x, node.path_y, 'y-')
+
+        for obs in self.obstacleList:
+            plt.gca().add_patch(plt.Rectangle((obs.x1,obs.y1),obs.size,obs.size, fc = 'blue', ec='red'))
+        
+        plt.axis([self.minRand, self.maxRand, self.minRand, self.maxRand])
         plt.grid(True)
         plt.pause(0.01)
 
-    def plotObstacles(self, obstacleList):
-        '''
-        Plot the obstacles
-        Input:  obstacleList: list of obstacles as polygons [obstacle1, obstacle2, ...]
-        Output: None
-        '''
-        for (obstacle) in obstacleList:
-            if isinstance(obstacle, obstacleSquare):
-                plt.plot([obstacle.x1, obstacle.x1], [obstacle.y1, obstacle.y2], '-k')
-                plt.plot([obstacle.x1, obstacle.x2], [obstacle.y2, obstacle.y2], '-k')
-                plt.plot([obstacle.x2, obstacle.x2], [obstacle.y2, obstacle.y1], '-k')
-                plt.plot([obstacle.x2, obstacle.x1], [obstacle.y1, obstacle.y1], '-k')
-            elif isinstance(obstacle, obstacleCircle):
-                circle = plt.Circle((obstacle.x, obstacle.y), obstacle.radius, color='k')
-                plt.gcf().gca().add_artist(circle)
-        plt.pause(0.01)
+def main(): 
+    obstacle1 = obstacleSquare(12,5,5)
+    obstacle2 = obstacleSquare(20,20,10)
+    obstacle3 = obstacleSquare(32,40,8)
+    obstacle4 = obstacleSquare(35,10,7)
+    obstacleList = [obstacle1, obstacle2, obstacle3, obstacle4]
+    start = [2,2]
+    goal = [45, 43]
+    randArea = [0,50]
+    rrt = RRT_star(start, goal, obstacleList, randArea)
+    path = rrt.planning()
+    
+
+if __name__ == '__main__':
+    main()
+
+
+
