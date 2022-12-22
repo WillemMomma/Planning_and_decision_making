74688f64bee4f8d9380046bb2bd54c18dc7da114
jvanleuven <jvanleuven@gmail.com>
2022-12-22 09:43:51 +0100

    barn

--- model_predictive_control/MPC.py
+++ model_predictive_control/MPC.py
@@ -53,7 +53,7 @@ def mpcControl(error, N, xInit, xTarget):
     print("we are in MPC")
     
     weightInput = np.array([[1,0],[0,1]])    # Weight on the input
-    weightTracking = np.array([[10,0,0],[0,10,0],[0,0,10]]) # Weight on the tracking state
+    weightTracking = np.array([[100,0,0],[0,100,0],[0,0,10]]) # Weight on the tracking state
     
     cost = 0.
     constraints = []
@@ -237,5 +237,7 @@ def testerTotal():
 
 
 testerTotal()
+
+
     
 
